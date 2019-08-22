528f2d555fa4c55ef1aadd8e7fd1d9ae736f806a
unknown <sumair_ahmed4@yahoo.co.in>
2019-08-22 12:31:00 +1000

    Added moe unit tests

--- src/app/quotes/components/quotes.component.spec.ts
+++ src/app/quotes/components/quotes.component.spec.ts
@@ -26,4 +26,60 @@ describe('QuotesComponent', () => {
   it('should create Quotes component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should use quoteList from the service', () => {
+    const quoteService = fixture.debugElement.injector.get(QuoteService)
+    fixture.detectChanges()
+    expect(quoteService.getQuote()).toEqual(component.quoteList)
+  })
+
+  it('should create a new post', () => {
+    component.quoteText = 'I love this test'
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.innerHTML).toContain('I love this test')
+  })
+
+  it('should disable the button when textArea is empty', () => {
+    fixture.detectChanges()
+    const button = fixture.debugElement.query(By.css('button'))
+    expect(button.nativeElement.disabled).toBeTruthy()
+  })
+
+  it('should enable the button when textArea is not empty', () => {
+    component.quoteText = 'Test quote'
+    fixture.detectChanges()
+    const button = fixture.debugElement.query(By.css('button'))
+    expect(button.nativeElement.disabled).toBeFalsy()
+  })
+
+  it('should remove post on card click', () => {
+    component.quoteText = 'This is a fresh post'
+    fixture.detectChanges()
+
+    fixture.debugElement
+      .query(By.css('.row'))
+      .query(By.css('.card'))
+      .triggerEventHandler('click', null)
+
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.innerHTML).toContain('This is a fresh post')
+  })
+
+  it('should fetch data asynchronously', () => {
+    const fakedFetchedList = [
+      new QuoteModel('I love unit testing', 'Mon 4, 2018')
+    ]
+
+    const quoteService = fixture.debugElement.injector.get(QuoteService)
+    let spy = spyOn(quoteService, 'fetchQuotesFromServer').and.returnValue(
+      Promise.resolve(fakedFetchedList)
+    )
+    fixture.detectChanges()
+    fixture.whenStable().then(() => {
+      expect(component.fetchedList).toBe(fakedFetchedList)
+    })
+  })
+
+
 });
